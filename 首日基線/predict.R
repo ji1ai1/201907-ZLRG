b5c4866fb64ef541709523470cee11ac185597c1
姬哀 <19585544+ji1ai1@users.noreply.github.com>
2020-11-08 00:10:16 +0800

    Update predict.R

--- 首日基線/predict.R
+++ 首日基線/predict.R
@@ -1,5 +1,10 @@
 #R version 3.6.1 (2019-07-05) -- "Action of the Toes"
 #R package data.table 1.12.2
+#輸入：
+#	zhaopin_round1_train_20190716/table3_action
+#	zhaopin_round1_user_exposure_A_20190723
+#輸出：
+#	result.csv
 #
 library(data.table)
 
